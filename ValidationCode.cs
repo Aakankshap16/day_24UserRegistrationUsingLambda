6aacb9e08359010032fb6617aad9a0f5cdd94574
Aakankshap16 <aakankshap971@gmail.com>
2023-04-13 22:08:29 +0530

    check email with condition

--- ValidationCode.cs
+++ ValidationCode.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace UserRegistrationUsingLambda
 {
     public class ValidationCode
     {
+        //FIRST NAME
         public void ValidateFirstName()
         {
             Func<string, bool> FirstName = name => name.Length >= 3 && char.IsUpper(name[0]);
@@ -23,6 +20,7 @@ namespace UserRegistrationUsingLambda
             }
         }
 
+        //LAST NAME
         public void ValidateLastName()
         {
             Func<string, bool> LastName = name => name.Length >= 3 && char.IsUpper(name[0]);
@@ -37,5 +35,33 @@ namespace UserRegistrationUsingLambda
                 Console.WriteLine("Invalid last name. Please enter a last name starting with a capital letter and at least 3 characters.");
             }
         }
+
+        // EMAIL ADDRESS
+        public void ValidateEmail()
+        {
+            Func<string, bool> Email = email1 => 
+            {
+                string pattern = @"^[0-9a-z._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$";
+                Regex regex = new Regex(pattern);
+                return regex.IsMatch(email1);
+            };
+
+            Console.Write("Please enter your email address: ");
+            string email = Console.ReadLine();
+            if (Email(email) == true)
+            {
+                Console.WriteLine("Valid email!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email. Please enter a valid email address.");
+            }
+        }
+
+
+
+
+
+
     }
 }
