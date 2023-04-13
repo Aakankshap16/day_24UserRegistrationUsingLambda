78a10a06e8ec4c87dae834ae4f0ff4481dd7181a
Aakankshap16 <aakankshap971@gmail.com>
2023-04-13 21:42:10 +0530

    check last name

--- ValidationCode.cs
+++ ValidationCode.cs
@@ -22,5 +22,20 @@ namespace UserRegistrationUsingLambda
                 Console.WriteLine("Invalid first name. Please enter a first name starting with a capital letter and at least 3 characters.");
             }
         }
+
+        public void ValidateLastName()
+        {
+            Func<string, bool> LastName = name => name.Length >= 3 && char.IsUpper(name[0]);
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+            if (LastName(lastName) == true)
+            {
+                Console.WriteLine("Valid last name!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid last name. Please enter a last name starting with a capital letter and at least 3 characters.");
+            }
+        }
     }
 }
