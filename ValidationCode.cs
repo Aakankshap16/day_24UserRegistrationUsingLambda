21228a4017fa37be46c7371dca59395c88a2b34c
Aakankshap16 <aakankshap971@gmail.com>
2023-04-13 21:30:24 +0530

    check first name

---
+++ ValidationCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class ValidationCode
+    {
+        public void ValidateFirstName()
+        {
+            Func<string, bool> FirstName = name => name.Length >= 3 && char.IsUpper(name[0]);
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+            if (FirstName(firstName) == true)
+            {
+                Console.WriteLine("Valid first name!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name. Please enter a first name starting with a capital letter and at least 3 characters.");
+            }
+        }
+    }
+}
